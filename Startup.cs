c6d6d3e96c66b54043a6e00986ab01fa79f5a92a
Sebastian Manriquez <seba.manriquezh@gmail.com>
2021-11-30 17:09:36 -0300

    swager

--- Startup.cs
+++ Startup.cs
@@ -131,12 +131,12 @@ public class Startup
         });
         #endregion
 
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
+        //if (env.IsDevelopment())
+        //{
+            //app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API-TPC v1"));
-        }
+        //}
 
         app.UseHttpsRedirection();
 
